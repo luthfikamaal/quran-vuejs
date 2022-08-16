2fde051c369d9d357a354ecc81a8c53423639bed
Muhammad Luthfi Kamal <90312645+luthfikamaal@users.noreply.github.com>
2022-08-16 09:42:54 +0700

    Update Surah.vue

--- src/views/Surah.vue
+++ src/views/Surah.vue
@@ -4,9 +4,9 @@
     <div class="w-full" id="card">
       <div class="grid">
         <div class="card pb-4 mb-3 text-right" v-for="(ayah, index) in surah.ayahs" :key="index">
-          <div class="card-body px-4">
-            <div class="mb-3 text-left text-2xl">{{ ayah.number }}</div>
-            <div class="text-arab-ayah text-2xl leading-arab-ayah">{{ ayah.text }}</div>
+          <div class="card-body">
+            <div class="mb-3 text-left text-2xl mx-4">{{ ayah.number }}</div>
+            <div class="text-arab-ayah text-2xl leading-arab-ayah mx-4">{{ ayah.text }}</div>
           </div>
         </div>
       </div>
